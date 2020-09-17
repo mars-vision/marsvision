17238cb98df9d70bda1125c5038b191e93919b69
Daniel <dpalencia2112@gmail.com>
2020-09-16 19:58:07 -0700

    Set up means & tests for feature extractor

--- marsvision/pipeline/FeatureExtractor.py
+++ marsvision/pipeline/FeatureExtractor.py
@@ -6,30 +6,30 @@ class FeatureExtractor:
     def __init__(self, detector):
         self.alg = detector
 
-    def getKeypointPoints(self, img):
+    def get_keypoint_points(self, img):
         keypoints = self.alg.detect(img)
         points = [(round(keypoint.pt[0]), round(keypoint.pt[1])) for keypoint in keypoints]
         return points
 
-    def selectROI(self, img, point, r):
+    def select_roi(self, img, point, r):
         rowStart = max(0, point[0] - r)
         rowEnd = min(img.shape[0] - 1, point[0] + r)
         colStart = max(0, point[1] - r)
         colEnd = min(img.shape[1] - 1, point[1] + r)
         return img[rowStart : rowEnd, colStart : colEnd]
 
-    def extractFeatures(self, img):
+    def extract_features(self, img):
         # Apply filters to each ROI
         # Reduce to means and variances (our features)
         # return a vector of features for each one.
-        points = self.getKeypointPoints(img)
+        points = self.get_keypoint_points(img)
         r = 20
 
         # todo: try averaging values for each keypoint instead
         # in a single vector
         featureMatrix = np.empty((len(points), 4))
         for i in range(len(points)):
-            roi = self.selectROI(img, points[i], r)
+            roi = self.select_roi(img, points[i], r)
             canny = cv2.Canny(roi, 50, 100)
             lapl = cv2.Laplacian(roi, cv2.CV_64F)
             vector = [
@@ -40,5 +40,10 @@ class FeatureExtractor:
             ]
             featureMatrix[i] = vector
         return featureMatrix
-            
+
+    def extract_means(self, img):
+        featureMatrix = self.extract_features(img)
+        featureMeans = np.mean(featureMatrix, axis=0)
+        return featureMeans
+
         
--- test/test_feature_extractor.py
+++ test/test_feature_extractor.py
@@ -6,12 +6,27 @@ import os
 import numpy as np
 
 class TestFeatureExtractor(TestCase):
-    def test_single_image(self):
-        # Good way of joining file paths
+    def setUp(self):
+        # Load up the array of expected features 
+        # for our test image from our marsFeatures.npy file
         dirname = os.path.dirname(__file__)
-        filepath = os.path.join(dirname, "img", "marsface.jpg")
+        featurePath = os.path.join(dirname, "marsFeatures.npy")
+        imagePath = os.path.join(dirname, "marsface.jpg")
+        self.img = cv2.imread(imagePath)
+        self.marsFeaturesKAZE = np.load(featurePath)
+        self.featureMeans = np.mean(self.marsFeaturesKAZE, axis=0)
+
+    def test_single_image_matrix_KAZE(self):
+        # Extract features, compare to expected features
         detector  = cv2.KAZE_create()
-        img = cv2.imread(filepath)
         extractor = FeatureExtractor(detector)
-        features = extractor.extractFeatures(img)
-        print(features, np.shape(features))
+        testFeatures = extractor.extract_features(self.img)
+        self.assertTrue(np.array_equal(self.marsFeaturesKAZE, testFeatures))
+    
+    def test_single_image_means_KAZE(self):
+        # Extract feature means, compare to expected feature means
+        detector  = cv2.KAZE_create()
+        extractor = FeatureExtractor(detector)
+        testMeans = extractor.extract_means(self.img)
+        self.assertTrue(np.array_equal(self.featureMeans, testMeans))
+
