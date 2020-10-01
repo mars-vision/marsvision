09ba44ab8f3d02fc5019f0f2c303f52fc70b6e7f
Daniel <dpalencia2112@gmail.com>
2020-10-01 00:33:45 -0700

    Refactor, add static feature extraction method
    Revert "Refactor, add static feature extraction method"
    
    This reverts commit 307f806de4a3668945d32ea59ece3a3e2acce23e.
    
    Auto stash before revert of "Refactor, add static feature extraction method"
    Auto stash before merge of "issue/2-loader-utility" and "origin/issue/2-loader-utility"
    Auto stash before merge of "issue/2-loader-utility" and "origin/issue/2-loader-utility"
    Refactor, add static feature extraction method

--- marsvision/pipeline/FeatureExtractor.py
+++ marsvision/pipeline/FeatureExtractor.py
@@ -1,35 +1,32 @@
 import numpy as np
 import cv2
-# Constructor:
-# Keypoint detector
+
+
 class FeatureExtractor:
     def __init__(self, detector):
         self.alg = detector
 
     def get_keypoint_points(self, img):
         keypoints = self.alg.detect(img)
-        points = [(round(keypoint.pt[0]), round(keypoint.pt[1])) for keypoint in keypoints]
-        return points
+        return [(round(keypoint.pt[0]), round(keypoint.pt[1])) for keypoint in keypoints]
 
     def select_roi(self, img, point, r):
-        rowStart = max(0, point[0] - r)
-        rowEnd = min(img.shape[0] - 1, point[0] + r)
+        row_start = max(0, point[0] - r)
+        row_end = min(img.shape[0] - 1, point[0] + r)
         colStart = max(0, point[1] - r)
         colEnd = min(img.shape[1] - 1, point[1] + r)
-        return img[rowStart : rowEnd, colStart : colEnd]
+        return img[row_start : row_end, colStart : colEnd]
 
-    def extract_features(self, img):
+    def extract_matrix_keypoints(self, img):
         # Apply filters to each ROI
-        # Reduce to means and variances (our features)
+        # Reduce to means and variances
         # return a vector of features for each one.
         points = self.get_keypoint_points(img)
-        r = 20
+        radius = 20
+        feature_matrix = np.empty((len(points), 4))
 
-        # todo: try averaging values for each keypoint instead
-        # in a single vector
-        featureMatrix = np.empty((len(points), 4))
         for i in range(len(points)):
-            roi = self.select_roi(img, points[i], r)
+            roi = self.select_roi(img, points[i], radius)
             canny = cv2.Canny(roi, 50, 100)
             lapl = cv2.Laplacian(roi, cv2.CV_64F)
             vector = [
@@ -38,12 +35,33 @@ class FeatureExtractor:
                 np.mean(lapl),
                 np.var(lapl)
             ]
-            featureMatrix[i] = vector
-        return featureMatrix
+            feature_matrix[i] = vector
 
-    def extract_means(self, img):
-        featureMatrix = self.extract_features(img)
-        featureMeans = np.mean(featureMatrix, axis=0)
-        return featureMeans
+        return feature_matrix
+
+    def extract_means_keypoints(self, img):
+        feature_matrix = self.extract_features(img)
+        feature_means = np.mean(feature_matrix, axis=0)
+        return feature_means
+
+    @staticmethod
+    def extract_features(img):
+        # Extract features from images without keypoints.
+        # Use the raw image, and extract these features.
+        img = np.array(img)
+        canny = cv2.Canny(img, 50, 100)
+        lapl = cv2.Laplacian(img, cv2.CV_64F)
+        feature_vector = [
+            np.mean(canny),
+            np.var(canny),
+            np.mean(lapl),
+            np.var(lapl),
+            np.mean(img),
+            np.var(img)
+        ]
+        return feature_vector
+
+
+
 
         
--- test/test_feature_extractor.py
+++ test/test_feature_extractor.py
@@ -1,32 +1,48 @@
 # Test cases for feature extractor
 from unittest import TestCase
-from marsvision.pipeline import FeatureExtractor
+from marsvision.pipeline import FeatureExtractor as fe
 import cv2
 import os
 import numpy as np
 
 class TestFeatureExtractor(TestCase):
     def setUp(self):
-        # Load up the array of expected features 
+        # Load up the matrix of expected features from keypoints
         # for our test image from our marsFeatures.npy file
         dirname = os.path.dirname(__file__)
-        featurePath = os.path.join(dirname, "marsFeatures.npy")
-        imagePath = os.path.join(dirname, "marsface.jpg")
-        self.img = cv2.imread(imagePath)
-        self.marsFeaturesKAZE = np.load(featurePath)
-        self.featureMeans = np.mean(self.marsFeaturesKAZE, axis=0)
+        image_path = os.path.join(dirname, "marsface.jpg")
+        self.img = cv2.imread(image_path)
 
-    def test_single_image_matrix_KAZE(self):
+        # take the means of the columns to reduce the feature matrix to a single vector
+        keypoint_feature_path = os.path.join(dirname, "marsFeatures.npy")
+        self.expected_feature_matrix_KAZE = np.load(keypoint_feature_path)
+        self.expected_keypoint_features = np.mean(self.expected_feature_matrix_KAZE, axis=0)
+
+        # Expected features for the full marsface.jpg test image
+        self.expected_features = [
+            22.536013333862456, 
+            5238.8115031509, 
+            -0.0013294178340410333, 
+            58.33776957278075, 
+            123.72320330171833, 
+            3634.7277409035864
+        ]
+
+    def test_keypoint_feature_matrix_KAZE(self):
         # Extract features, compare to expected features
         detector  = cv2.KAZE_create()
-        extractor = FeatureExtractor(detector)
-        testFeatures = extractor.extract_features(self.img)
-        self.assertTrue(np.array_equal(self.marsFeaturesKAZE, testFeatures))
+        extractor = fe(detector)
+        test_features = extractor.extract_matrix_keypoints(self.img)
+        self.assertTrue(np.array_equal(self.expected_feature_matrix_KAZE, test_features))
     
-    def test_single_image_means_KAZE(self):
+    def test_keypoint_feature_vector_KAZE(self):
         # Extract feature means, compare to expected feature means
-        detector  = cv2.KAZE_create()
-        extractor = FeatureExtractor(detector)
-        testMeans = extractor.extract_means(self.img)
-        self.assertTrue(np.array_equal(self.featureMeans, testMeans))
+        detector = cv2.KAZE_create()
+        extractor = fe(detector)
+        test_means = extractor.extract_means_keypoints(self.img)
+        print(self.expected_keypoint_features, test_means)
+        self.assertTrue(np.array_equal(self.expected_keypoint_features, test_means))
 
+    def test_feature_vector(self):
+        test_feature_vector = fe.extract_features(self.img)
+        self.assertTrue(np.array_equal(self.expected_features, test_feature_vector))
