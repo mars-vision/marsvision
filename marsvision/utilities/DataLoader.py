eed9b84c21e65353087a39315fa871848bac747e
Daniel <dpalencia2112@gmail.com>
2020-10-13 00:58:47 -0700

    Add file name reading, write tests, write docstrings

--- marsvision/utilities/DataLoader.py
+++ marsvision/utilities/DataLoader.py
@@ -10,13 +10,14 @@ class DataLoader:
     def __init__(self, 
             in_path: str = None,
             out_path: str = None, 
-            class_name: str = None): 
+            class_name: str = None,
+            include_filename: bool = False): 
         """
             This class is responsible for loading images from an input directory,
             extracting features from them,
             and outputting the processed data as a .csv file. 
             
-            A class parameter can be used to classify all images within the inoput folder.
+            A class parameter can be used to classify all images within the input folder.
 
             The csv file will be appended if one already exists, so this script can be invoked on 
             multiple folders to produce an output file with many classes.
@@ -28,7 +29,8 @@ class DataLoader:
             ----------
             in_path (str): Optional. The input directory which contains images to be read. Reads from the current working directory if left empty.
             out_path (str): Optional. The output directory to which the csv will be written. Writes to current working directory if left empty.
-            class_name (str): Optional. A class name for the input. 
+            class_name (str): Optional. A class name for the input.
+            include_filename(bool): Optional. Whether to include the file name. False by default. 
 
         """
         self.class_name = class_name
@@ -42,37 +44,73 @@ class DataLoader:
             self.out_path = os.getcwd()
         else: 
             self.out_path = out_path
+
+        self.include_filename = include_filename
+
     def data_reader(self):
+        """
+            Load images and file names as members.
+            
+            All .jpg images in the working directory,
+            and all subdirectories are loaded.
+
+            This function updates the self.images,
+            and self.file_names members with the loaded data.
+
+        """
+
         # Use the walk function to step through each folder,
         # and save all jpg files into a list.
         images = []
+        file_names = []
         walk = os.walk(self.in_path, topdown=True)
         for(root, dirs, files) in walk:
             for file in files:
                 if file.endswith(".jpg"):
                     images.append(cv2.imread(os.path.join(root, file)))
+                    file_names.append(file)
         self.images = images
+        self.file_names = file_names
 
     def data_transformer(self):
+        """
+            Use the FeatureExtractor module to load
+            a vector of features into memory as a member variable, as defined in that class.
+        """
         # Use the feature extractor to produce 
         # a list of feature vectors.
         detector  = cv2.ORB_create()
         extractor = FeatureExtractor(detector)
-        self.featureList = [extractor.extract_means(image) for image in self.images]
-        
+        self.feature_list = [extractor.extract_features(image) for image in self.images]
 
     def data_writer(self):
+        """
+            Creates a Pandas dataframe from the extracted features,
+            adds extra columns depending on user preferences:
+            
+            If a class is defined, write to the class to a class column.
+            If file names are desired, file names are written to a file_name column.
+        """
         # Write features to CSV with path names.
         # Use the feature extractor to retrieve features from images.
-        df = pd.DataFrame(data = self.featureList)
-        if self.class_name is not None:
-            df["class"] = class_name
+        df = pd.DataFrame(data = self.feature_list)
         
-        out_file = os.path.join(self.out_path, "output.csv")
+        if self.class_name is not None:
+            df["class"] = self.class_name
+        if self.include_filename:
+            df["file_name"] = self.file_names
 
+        out_file = os.path.join(self.out_path, "output.csv")
         df.to_csv(out_file, mode="a")
 
     def run(self):
+        """
+            When called, run() will execute the data_reader(), data_transformer(), and data_writer() 
+            functions in order.
+
+            The result will be an output .csv file either in the directory from which the script is run,
+            or in the user specified directory.
+        """
         self.data_reader()
         self.data_transformer()
         self.data_writer()
@@ -85,8 +123,9 @@ if __name__ == "__main__":
     parser.add_argument("--i", help="Input directory")
     parser.add_argument("--o", help="Output directory")
     parser.add_argument("--c", help="Class for input files")
+    parser.add_argument("--f", help="Boolean on whether to include the file name or not.")
     args = parser.parse_args()
-    loader = DataLoader(args.i, args.o, args.c)
+    loader = DataLoader(args.i, args.o, args.c, args.f)
     loader.run()
 
 
