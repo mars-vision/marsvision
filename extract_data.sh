b85e27fc8768a3ec243f9eab9f9a8343c274ce6d
Daniel <dpalencia2112@gmail.com>
2020-10-27 01:41:45 -0700

    Update extract_data script; default argument, call python script directly

--- extract_data.sh
+++ extract_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-conda activate marsvision
+dir_path=$(dirname $(realpath $0))
 cd mars-dataset
-python -m marsvision.utilities.DataLoader --i="dust" --c="dust" --f=True
-python -m marsvision.utilities.DataLoader --i="featureless" --c="no_dust" --f=True
-python -m marsvision.utilities.DataLoader --i="good" --c="no_dust" --f=True
-python -m marsvision.utilities.DataLoader --i="miscal" --c="no_dust" --f=True
+python $dir_path/marsvision/utilities/DataLoader.py --i=dust --c=dust --f
+python $dir_path/marsvision/utilities/DataLoader.py --i=featureless --c=no_dust --f
+python $dir_path/marsvision/utilities/DataLoader.py --i=good --c=no_dust --f
+python $dir_path/marsvision/utilities/DataLoader.py --i=miscal --c=no_dust --f
