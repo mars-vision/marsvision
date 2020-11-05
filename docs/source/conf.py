4bdd3c66227891119feb060453bf1fa8128906b7
Daniel <dpalencia2112@gmail.com>
2020-11-05 10:09:06 -0800

    Merge in dust classifier files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'marsvision'
-copyright = '2020'
+copyright = '2020 Michael Munje, Mariajose Corral, Iris Rafaela, Daniel Palencia'
 author = 'Michael Munje, Mariajose Corral, Iris Rafaela, Daniel Palencia'
 
 # The full version, including alpha/beta/rc tags
