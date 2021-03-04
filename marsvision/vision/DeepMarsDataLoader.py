228896de2ec2cd8091c61e7315995cb1163b5329
Daniel <dpalencia2112@gmail.com>
2021-03-03 20:48:23 -0800

    Add PyTorch DataLoader for deep mars + unit test
    Add PyTorch DataLoader for deep mars + unit test

---
+++ marsvision/vision/DeepMarsDataLoader.py
@@ -0,0 +1,54 @@
+import os
+import cv2
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torch import Tensor
+
+class DeepMarsDataLoader(Dataset):
+    """
+        Custom Pytorch DataLoader set up to work with the Deep Mars dataset.
+        
+    """
+    # Wrapper to work with deep mars dataset
+    def __init__(self, root_dir):
+        
+        # AlexNet expects images to be normalized this way.
+        self.normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        )
+        # Get image labels
+        self.labels = {}
+        with open(os.path.join(root_dir, "labels-map-proj.txt")) as f:
+            for line in f:
+                items = line.split()
+                key, value = items[0], items[1]
+                self.labels[key] = int(value)
+                
+        # Get image filenames
+        self.image_dir = os.path.join(root_dir, "map-proj")
+        image_names = os.listdir(os.path.join(self.image_dir))
+        # Take set difference 
+        # to ensure that only labelled images are included
+        self.image_names = list(set(image_names) & set(self.labels))
+                                  
+    def __getitem__(self, idx):
+        # Get a sample as: {'image': image, 'label': label}
+        # Return an image with the dimensions 3 x W x H
+        # Because PyTorch models expect these dimensions as inputs.
+        # Transpose dimensions:
+        # (W, H, 3) --> (3, W, H)
+        img_name = self.image_names[idx]
+        img = Tensor(
+            cv2.imread(os.path.join(self.image_dir, img_name))
+        ).transpose(0, 2)
+        
+        # Apply normalize
+        img = self.normalize(img)
+    
+        return {
+            "image": img,
+            "label": self.labels[self.image_names[idx]]
+        }
+        
+    def __len__(self):
+        return len(self.image_names)
---
+++ marsvision/vision/__init__.py
@@ -0,0 +1,1 @@
+from .DeepMarsDataLoader import DeepMarsDataLoader
