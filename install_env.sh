1e3b06fac2d0ccf1a3d061e4d085c17420237b48
Daniel <dpalencia2112@gmail.com>
2021-04-27 20:43:45 -0700

    Update install script

--- install_env.sh
+++ install_env.sh
@@ -17,7 +17,8 @@ cd pdsc
 pip install .
 mkdir pdsc_tables
 cd pdsc_tables
-curl https://hirise-pds.lpl.arizona.edu/PDS/INDEX/RDRCUMINDEX.TAB > /dev/null >2&1
-curl https://hirise-pds.lpl.arizona.edu/PDS/INDEX/RDRCUMINDEX.LBL > /dev/null >2&1
+touch output.txt
+curl https://hirise-pds.lpl.arizona.edu/PDS/INDEX/RDRCUMINDEX.TAB -o RDRCUMINDEX.TAB
+curl https://hirise-pds.lpl.arizona.edu/PDS/INDEX/RDRCUMINDEX.LBL -o RDRCUMINDEX.LBL
 cd ..
 pdsc_ingest ./pdsc_tables/RDRCUMINDEX.LBL ./pdsc_tables
