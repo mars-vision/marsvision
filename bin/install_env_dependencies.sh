29d859a8cd07c3f8e357f592b88925a10b26ea28
Michael Munje <mmunje3@gatech.edu>
2022-09-28 00:28:28 -0400

    Fix environment.yml and installation script.

--- bin/install_env_dependencies.sh
+++ bin/install_env_dependencies.sh
@@ -2,12 +2,15 @@
 set -e
 
 # Packages installation
-conda create --name marsvision -f environment.yml
+conda env create --name marsvision --file=environment.yml
 
 # PDSC Installation
+rm -rf ../data/pdsc_tables
 mkdir ../data/pdsc_tables
 cd ../data/pdsc_tables
 curl https://hirise-pds.lpl.arizona.edu/PDS/INDEX/RDRCUMINDEX.TAB -o RDRCUMINDEX.TAB
 curl https://hirise-pds.lpl.arizona.edu/PDS/INDEX/RDRCUMINDEX.LBL -o RDRCUMINDEX.LBL
 cd ..
+eval "$(conda shell.bash hook)"
+conda activate marsvision
 pdsc_ingest ./pdsc_tables/RDRCUMINDEX.LBL ./pdsc_tables
